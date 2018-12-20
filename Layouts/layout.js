5537b26d52f70ea2b2f8f1b85900875568963ca6
adelbalso <a.delbalso@gmail.com>
2018-12-20 16:33:51 -0500

    global loader

--- Layouts/layout.js
+++ Layouts/layout.js
@@ -2,14 +2,38 @@ import "./styles.scss"
 import Link from 'next/link'
 import Head from 'next/head'
 
-export default ({ children, title = 'Little Universe' }) => (
-  <div>
-    <Head>
-      <title>{ title }</title>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
+class Layout extends React.Component {
+  state = {
+    loading: true,
+  }
 
-    { children }
-  </div>
-)
+  componentDidMount() {
+    setTimeout(function() {
+      this.setState({ loading: false });
+    }.bind(this), 1000);
+  };
+
+  render() {
+    let { children, title } = this.props
+
+    return (
+      <div>
+        <Head>
+          <title>{ title }</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+
+        {
+          this.state.loading ?
+          <h1>LOADING MOTHERFUCKERS</h1>
+          :
+          <div className="pageContent">{children}</div>
+        }
+
+      </div>
+    )
+  }
+}
+
+export default Layout
