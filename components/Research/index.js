3790b984bbda76f3a7eeb26842d07593b8fbd7a7
taylorz <taylor.zanke@gmail.com>
2018-12-21 16:52:15 -0700

    research blurbs

--- components/Research/index.js
+++ components/Research/index.js
@@ -6,16 +6,19 @@ export default () =>
     <Grid container className="research" spacing={24} direction="column">
       <Grid item>
         <Grid container spacing={24} direction="column">
-          <Grid item>
+          <Grid item xs={6}>
             <h1>Motion.js and the Non-Nested Web</h1>
+            <p>Motion.js is a tool that we'll use to do some great stuff. </p>
           </Grid>
 
-          <Grid item>
+          <Grid item xs={6}>
             <h1>Wireframing and Mocking It Up</h1>
+            <p>Working with complex flows and lots of user inputs makes wireframing and mockups so important. This is how we do it!</p>
           </Grid>
 
-          <Grid item>
+          <Grid item xs={6}>
             <h1>Writing the Motion.js API</h1>
+            <p>Motion.js is a tool that we'll use to do some great stuff. </p>
           </Grid>
         </Grid>
       </Grid>
