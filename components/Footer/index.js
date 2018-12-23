6bfcda87d39a6be7741e87e67ac18c6665f0da9f
taylorz <taylor.zanke@gmail.com>
2018-12-23 14:36:39 -0700

    social links footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,8 +6,23 @@ import NavLink from '../ui/NavLink'
 
 export default () =>
   <div className="footer container">
-    <p className="secondary">260 Ainslie Street, 3rd Floor</p>
-    <p className="secondary">Brooklyn, NY 11211</p>
-    <br />
-    <p className="secondary">agency@littleuniverse.com</p>
+    <Grid container>
+      <Grid item xs={3} className="footerItem agencyName">
+        <p className="secondary">Little Universe</p>
+      </Grid>
+      <Grid item xs={3} className="footerItem agencyInfo">
+        <p className="secondary">260 Ainslie Street, 3rd Floor</p>
+        <p className="secondary">Brooklyn, NY 11211</p>
+        <br />
+        <p className="secondary">agency@littleuniverse.com</p>
+      </Grid>
+      <Grid item xs={3} className="footerItem">
+        <p></p>
+      </Grid>
+      <Grid item xs={3} className="footerItem social">
+        <p className="secondary"><a href="" className="socialLink">Twitter</a></p>
+        <p className="secondary socialLink"><a href="" className="socialLink">LinkedIn</a></p>
+        <p className="secondary socialLink"><a href="" className="socialLink">Instagram</a></p>
+      </Grid>
+    </Grid>
   </div>
