fbb880bd856194ec42aee26fb961bafa67ac39dd
taylorz <taylor.zanke@gmail.com>
2019-01-21 11:03:52 -0500

    changed the footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -26,10 +26,10 @@ class Footer extends React.Component {
     return (
       <div className="footer container">
         <Grid container>
-          <Grid item xs={3} className="footerItem agencyName">
+          <Grid item xs={6} className="footerItem agencyName">
             <p className="secondary">Little Universe</p>
           </Grid>
-          <Grid item xs={3} className="footerItem agencyInfo">
+          <Grid item xs={6} className="footerItem agencyInfo">
             <p className="secondary">260 Ainslie Street, 3rd Floor</p>
             <p className="secondary">Brooklyn, NY 11211</p>
             <br />
@@ -42,14 +42,6 @@ class Footer extends React.Component {
               </CopyToClipboard>
               <p className={ this.state.copiedText ? "copiedText" : "copiedText fadeOut"}>Copied</p>
             </Grid>
-          </Grid>
-          <Grid item xs={3} className="footerItem">
-            <p></p>
-          </Grid>
-          <Grid item xs={3} className="footerItem social">
-            <p className="secondary"><a href="" className="socialLink">Twitter</a></p>
-            <p className="secondary socialLink"><a href="" className="socialLink">LinkedIn</a></p>
-            <p className="secondary socialLink"><a href="" className="socialLink">Instagram</a></p>
           </Grid>
         </Grid>
       </div>
