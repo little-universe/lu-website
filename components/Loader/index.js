eb8700c4b420f256193e1e1447a8d5a7818e0ee5
adelbalso <a.delbalso@gmail.com>
2019-01-21 10:48:05 -0500

    ...

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -2,12 +2,10 @@ import "./styles.scss"
 import Link from 'next/link'
 import Grid from '@material-ui/core/Grid'
 import Logo from '../ui/Logo'
-import Nav from '../Nav'
+import NavLink from '../ui/NavLink'
 
 export default () =>
-  <>
-    <Grid className="loader" container>
-      <div className="spinner" />
-      <div className="spinner2" />
-    </Grid>
-  </>
+  <Grid className="loader" container>
+    <div className="spinner" />
+    <div className="spinner2" />
+  </Grid>
