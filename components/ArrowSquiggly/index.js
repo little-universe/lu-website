16cc73105d59930051a88afc005058ec0ea2bee7
adelbalso <a.delbalso@gmail.com>
2019-01-25 23:10:39 -0500

    squiggly animation

--- components/ArrowSquiggly/index.js
+++ components/ArrowSquiggly/index.js
@@ -2,12 +2,13 @@ import "./styles.scss"
 
 export default ({ className }) =>
   <span className={className} >
-    <svg width="243px" height="19px" viewBox="0 0 243 19" version="1.1">
-        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="arrow-squiggly" transform="translate(0.000000, -2.000000)" stroke="#555E68" stroke-width="3">
-                <path d="M0,8.41908012 C52.8523614,8.41908012 66.0347031,19 107.815265,19 C124.870408,19 131.01983,4 169.281138,4 C206.277889,4 227.427809,11.5 243,11.5" id="Path-5"></path>
-            </g>
-        </g>
+
+    <svg width="86px" height="13px" viewBox="0 0 86 13" version="1.1" className="squiggly-svg">
+      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="arrow-squiggly" transform="translate(0.000000, -5.000000)" stroke="#555E68" stroke-width="3">
+              <path d="M0,9.65144807 C18.7049509,9.65144807 23.3703064,16 38.1568427,16 C44.1928192,16 46.369158,7 59.9101968,7 C73.0036974,7 80.4888543,11.5 86,11.5" id="Path-5"></path>
+          </g>
+      </g>
     </svg>
 
   </span>
