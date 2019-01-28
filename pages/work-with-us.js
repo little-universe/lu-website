3c58a368975bc366e8ecca232987fc4c69487483
taylorz <taylor.zanke@gmail.com>
2019-01-28 13:46:42 -0500

    work with page

--- pages/work-with-us.js
+++ pages/work-with-us.js
@@ -7,22 +7,33 @@ import LeadForm from '../components/LeadForm'
 import LuHqMap from '../components/LuHqMap'
 
 export default () =>
-  <Layout title='Little Universe - Work With Us' className="workWithUs-page">
-    <div className="container">
-      <Grid container>
-        <Grid item md={6} xs={12} style={{ height: 500, width: "100%"}}>
-          <LuHqMap
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBmW_YvGkgxxJCikV8U41NvnCyFXESb5g8"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `100%` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
+  <>
+    <Layout title='Little Universe - Work With Us' className="workWithUs-page">
+    <Hero
+      className="hero-workWith"
+      headline={<>Work With Us</>}
+      subhead={["Drop us a line or give us a call. We're always excited to talk concept or talk shop."]}
+    />
+        <Grid container className="container visitUs"  spacing={40} alignItems="center">
+          <Grid item md={6} xs={12}>
+            <div className="visitItem visitUsContact">
+              <h3>Drop us a line.</h3>
+              <p>agency@littleuniverse.com</p>
+            </div>
+            <div className="visitItem visitUsAddress">
+              <h3>Come Visit.</h3>
+              <p>260 Ainslie Street, 3rd Floor <br/>Brooklyn, NY 11211</p>
+            </div>
+          </Grid>
+          <Grid item md={6} xs={12} style={{ height: 500, width: "100%"}}>
+            <LuHqMap
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBmW_YvGkgxxJCikV8U41NvnCyFXESb5g8"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `100%` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </Grid>
         </Grid>
-        <Grid item md={6}>
-        </Grid>
-      </Grid>
-    </div>
-
-    <LeadForm />
-    <Footer />
-  </ Layout>
+      <Footer />
+    </ Layout>
+  </>
