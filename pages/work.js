9e11232875ec804192246f735ddd3f769fe0256b
taylorz <taylor.zanke@gmail.com>
2019-01-28 15:47:22 -0500

    work page

--- pages/work.js
+++ pages/work.js
@@ -1,4 +1,5 @@
 import Layout from '../Layouts/layout'
+import Link from 'next/link'
 import Grid from '@material-ui/core/Grid'
 import Nav from '../components/Nav'
 import Hero from '../components/ui/Hero'
@@ -19,19 +20,27 @@ export default () =>
     />
     </div>
     <Grid container className="container workList" spacing={40} alignItems="center">
-      <Grid container className="containerCard" spacing={40} alignItems="center">
-        <Grid item md={6} xs={12} className="workProject workStrive">
-          <h2>Strive Digital</h2>
-          <p>See how we helped strive digital build a tool for text message campaigns.</p>
-          <img className="product-screenshot" src="../static/product-screenshots/strive/strive-1.png" />
-        </Grid>
-        <Grid item md={6} xs={12} className="workProject workBetterfin">
-          <h2>Betterfin</h2>
-          <p>See how we helped Betterfin get to a compelling MVP and redesigned application.</p>
-          <img className="product-screenshot" src="../static/product-screenshots/betterfin/betterfin-1.png" />
-        </Grid>
+      <Grid container className="containerCard workContainer" spacing={40} alignItems="center">
+        <Link href="/strive">
+          <Grid item md={6} xs={12} className="workProject workStrive">
+            <h2>Strive Digital</h2>
+            <p>See how we helped strive digital build a tool for text message campaigns.</p>
+            <img className="product-screenshot" src="../static/product-screenshots/strive/strive-1.png" />
+          </Grid>
+        </Link>
+        <Link href="/betterfin">
+          <Grid item md={6} xs={12} className="workProject workBetterfin">
+            <h2>Betterfin</h2>
+            <p>See how we helped Betterfin get to a compelling MVP and redesigned application.</p>
+            <img className="product-screenshot" src="../static/product-screenshots/betterfin/betterfin-1.png" />
+          </Grid>
+        </Link>
+      </Grid>
+      <Grid container className="container linksToWork" spacing={40} alignItems="center">
+      <Grid item md={6} xs={12} className="workLink"><a href="https://www.myxfitness.com/">Myx Fitness</a></Grid>
       </Grid>
     </Grid>
+
 
   <Footer />
 </ Layout>
