6962e421b2e80c397e34d54b4f44b774268af6c0
taylorz <taylor.zanke@gmail.com>
2019-02-04 11:27:25 -0500

    mobile logo fix

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -60,7 +60,7 @@ export default ({ theme, route }) =>
       return (
         <>
           <Link href="/">
-            <Grid item onClick={this.toggleMobileNav} xs={1} className="logoWrapper">
+            <Grid item onClick={this.toggleMobileNav} xs={1} className="logoWrapper mobileLogoWrapper">
               <Logo />
             </Grid>
           </Link>
