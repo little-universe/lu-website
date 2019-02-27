c6f4e20c2b922c7eaad281277145ea6c23d1ce9c
taylorz <taylor.zanke@gmail.com>
2019-02-27 15:12:58 -0500

    seo

--- Layouts/layout.js
+++ Layouts/layout.js
@@ -34,7 +34,7 @@ class Layout extends React.Component {
           <meta name="description" content="We're a digital product agency based in Brooklyn. Every product is its own Little Universe."/>
           <meta property="og:description" content="We're a digital product agency based in Brooklyn. Every product is its own Little Universe."/>
           <meta property="og:image" content="https://s3.us-east-2.amazonaws.com/lu-assets/little-universe-meta.png"/>
-          <meta property="keywords" content="little, universe, little universe, digital product, digital, product, product design, product agency, agency, interaction, interaction design, design, development, design and development, web, website, web design, startup, build, design and build"/>
+          <meta property="keywords" content="little, universe, little universe, digital product, digital, product, product design, product agency, agency, interaction, interaction design, design, development, design and development, web, website, web design, startup, build, design and build, brooklyn, new york city, new york, williamsburg"/>
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <link rel="shortcut icon" type="image/png" href="../static/lu-favicon.png"/>
